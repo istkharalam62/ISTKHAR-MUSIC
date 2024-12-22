1807243a0335a3dd8be409778b35e0211c26938b
𐏓꯭꯭꯭꯭꯭꯭꯭𝖳 ꯭꯭ʜ꯭꯭꯭ ߎ꯭ ꯭ɴ ꯭ᴅ꯭ ᴇ꯭ ʀ ꯭ꭗ꯭Ḋ꯭꯭ <mdistkhar601@gmail.com>
2024-12-22 13:09:47 +0530

    Update unbanall.py

--- ISTKHARMUSIC/plugins/admins/unbanall.py
+++ ISTKHARMUSIC/plugins/admins/unbanall.py
@@ -2,7 +2,7 @@ from ISTKHARMUSIC import app
 from config import OWNER_ID
 from pyrogram import filters, enums
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-from ISTKHARMUSIC.utils.ISTKHAR_ban import admin_filter
+from ISTKHARMUSIC.utils.istkhar_ban import admin_filter
 
 BOT_ID = app.id
 
